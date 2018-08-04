6507b23d1f8b1b28c485cb7a62191fb8e27b7e9c
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2018-08-04 02:14:50 -0500

    Correccion de guardado de entidades

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -34,6 +34,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>
 		Author eric = new Author("Eric", "Evans");
 		Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
 		eric.getBooks().add(ddd);
+		ddd.getAuthors().add(eric);
 
 		authorRepository.save(eric);
 		bookRepository.save(ddd);
@@ -42,6 +43,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>
 		Author rod = new Author("Rod", "Johnson");
 		Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox");
 		rod.getBooks().add(noEJB);
+		noEJB.getAuthors().add(rod);
 
 		authorRepository.save(rod);
 		bookRepository.save(noEJB);
